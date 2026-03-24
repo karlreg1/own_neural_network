5745590233cb00ee9a145ec9e966edb4d542cb0f
Karl <karl.rehagel@web.de>
2026-03-24 21:04:11 +0100

    started with backprob and added mean square loss for training later

--- mlp.py
+++ mlp.py
@@ -4,19 +4,40 @@ import matplotlib as plt
 class Layer():
     def __init__(self, input_size, output_size, activation_fct):
         self.activation_fct = activation_fct
-        self.weights = np.random.randn(output_size, input_size)
+        self.weights = np.random.randn(output_size, input_size) #weights are initialized randomly
         self.biases = np.zeros(output_size)
 
+    #calculates the whole activation for layer
     def forward(self, x):
-        pre_activation = np.dot(self.weights, x) + self.biases
-        activation = self.activation_fct.forward(pre_activation)
+        self.x = x
+        self.pre_activation = np.dot(self.weights, x) + self.biases
+        activation = self.activation_fct.forward(self.pre_activation)
         return activation
+    
+    def backward(self, delta):
+        delta_activation = self.activation_fct.backward(self.pre_activation) * delta
+        self.grad_weights = np.outer(delta_activation, self.x)
+        self.grad_biases = delta_activation
+        grad_input = self.weights.T @ delta_activation
+    
+        return grad_input
+
 
 
 class Relu():
     def forward(self, x):
         return np.maximum(0, x)
+    
+    def backward(self, x):
+        #converts the matrix x first into boolean values (x_i > 1 -> true else false) and converts them then into folating point numbers
+        return (x > 0).astype(float)
 
+class MSELoss():
+    def forward(self, y_pred, y_true):
+        return np.mean((y_true - y_pred)**2)
+    
+    def backward(self, y_pred, y_true):
+        return (2 / len(y_true)) * (y_pred - y_true)
 
 class MLP():
     def __init__(self):
@@ -31,8 +52,9 @@ class MLP():
             output = layer.forward(output)
         return output
 
-    def backward(self):
-        pass 
+    def backward(self, delta):
+        for layer in reversed(self.layers):
+            delta = layer.backward(delta)
 
 mlp = MLP()
 layer1 = Layer(3, 4, Relu())
---
+++ perceptron.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib as plt
+
+
+class Perceptron():
+    pass
+
