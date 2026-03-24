2bf16e6b58707474f52e28b1bbbaa483a0287f8f
Karl <karl.rehagel@web.de>
2026-03-24 18:06:46 +0100

    started the project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # own_neural_network
+
+This project is based on my university lecture on artificial neural networks. 
+I would like to create a working multilayer perceptron model and perhaps expand it further.
+The project should be programmed in Python, and I only want to use NumPy and Matplotlib.
---
+++ mlp.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib as plt
+
+class MLP():
+    def __init__(self, layer: list):
+        self.layer = layer
+        
+    def forward(self):
+        pass
+    def backward(self):
+        pass
+
+class Neuron():
+    def __init__(self):
+        pass
+    def calculate_activation(self):
+        pass
+
+class Relu():
+    def __init__(self):
+        pass
